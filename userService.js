593874d3e615ac61e10ceddbd67f43d1b02fc249
Tommy Brown <46692709+tbrowndev@users.noreply.github.com>
2020-08-10 22:29:43 -0500

    updated database configuration

--- userService.js
+++ userService.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 
 // Configuration
 var connection;
-const db_config = require('../db_config.json');;
+const db_config = require('./db_amazon_config.json');;
 
 app.use(bodyParser.urlencoded({ 'extended': 'true' }));
 app.use(bodyParser.json());
